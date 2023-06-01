afc4e4f3745a473672681a8975deb80206f07cb7
ahmedabdulgid <ahmedabdulgid@gmail.com>
2023-06-01 22:05:16 +0300

    Start Production

--- src/components/AppLogo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="app-logo">
-    <img src="../assets/images/logo.png" alt="Logo App">
-  </div>
-</template>
--- src/stores/useError.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-export const useError = defineStore("error", {
-  state: () => ({
-    masg: "",
-    style: "",
-    show: false,
-  }) 
-});
